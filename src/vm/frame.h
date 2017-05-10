71fdfba9de01f15f6818d2a0e831c86391b0b7a2
Hang Park <hangpark@kaist.ac.kr>
2017-05-10 23:24:23 +0900

    [#117] Implement FIFO page eviction

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -3,9 +3,10 @@
 
 #include <list.h>
 #include "threads/palloc.h"
+#include "vm/page.h"
 
 void frame_table_init (void);
-void *frame_alloc (enum palloc_flags);
+void *frame_alloc (struct suppl_pte *, enum palloc_flags);
 void frame_free (void *kpage);
 void frame_remove (void *kpage);
 
