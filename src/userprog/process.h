484bfcbc42107caeff14020e8b05d045cdd32e72
Hang Park <hangpark@kaist.ac.kr>
2017-05-20 23:17:51 +0900

    [#133] Implement memory mapped files

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -7,6 +7,12 @@
 typedef int pid_t;
 #define PID_ERROR ((pid_t) -1)      /* Error value for pid_t. */
 
+#ifdef VM
+/* Map region identifier. */
+typedef int mapid_t;
+#define MAP_FAILED ((mapid_t) -1)   /* Error value for mapid_t. */
+#endif
+
 /* Process status flags. */
 #define PROCESS_LOADING 0           /* Process is loading. */
 #define PROCESS_RUNNING 1           /* Process is running. */
@@ -21,8 +27,14 @@ struct process
     struct file *exec_file;         /* Process executable file. */
     struct list child_list;         /* List of child processes. */
     struct list file_list;          /* List of files in use. */
+#ifdef VM
+    struct list mmap_list;          /* List of memory mapped file. */
+#endif
     struct process_info *info;      /* Process information for its parent. */
     int fd_next;                    /* File descriptor tracker. */
+#ifdef VM
+    mapid_t mapid_next;             /* Mapping identifier tracker. */ 
+#endif
   };
 
 /* An user process information for its parent process. */
@@ -44,13 +56,29 @@ struct process_file
     struct list_elem elem;          /* List element. */
   };
 
+#ifdef VM
+/* A memory mapped file information by some process. */
+struct process_mmap
+  {
+    mapid_t id;                     /* Mapping identifier. */
+    struct file *file;              /* Memory mapped file. */
+    void *addr;                     /* Mapped address. */
+    size_t size;                    /* File size. */
+    struct list_elem elem;          /* List element. */
+  };
+#endif
+
 pid_t process_execute (const char *file_name);
-int process_wait (pid_t pid);
+int process_wait (pid_t);
 void process_exit (void);
 void process_activate (void);
 struct process *process_current (void);
-struct process_info *process_find_child (pid_t pid);
+struct process_info *process_find_child (pid_t);
 struct file *process_get_file (int fd);
-int process_set_file (struct file *file);
+int process_set_file (struct file *);
+#ifdef VM
+struct process_mmap *process_get_mmap (mapid_t);
+mapid_t process_set_mmap (struct file *, void *addr, size_t);
+#endif
 
 #endif /* userprog/process.h */
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,7 +1,11 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "process.h"
+
 void syscall_init (void);
 void syscall_exit (int);
+
+void unmap_mmap_item (struct process_mmap *);
 
 #endif /* userprog/syscall.h */
